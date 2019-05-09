61d050439a07f68777f1d00944d5e057c163509e
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-05-09 02:46:31 -0700

    Add files via upload

---
+++ HackerEarth-Roy and Shopping.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// The number of ways , u have to choose cities at least one city so, obviously 2^n-1
+
+#define max 1000000007
+
+typedef unsigned long long int ll;
+
+#define mod 1000000007
+
+bool prime[1000001];
+int prime_div[1000001];
+
+void prime_sieve()
+{
+    memset(prime,true,sizeof(prime));
+    for(int i=2;i<=sqrt(1000000);i++)
+    {
+        if(prime[i])
+        {
+            for(int j=2*i;j<=1000000;j=j+i)
+            {
+                prime[j]=false;
+            }
+        }
+    }
+    prime[2]=true;
+    prime[1]=false;
+}
+void prime_div_min()
+{
+    memset(prime_div,0,sizeof(prime_div));
+    for(int i=2;i<=1000000;i++)
+    {
+        if(prime[i])
+        {
+            for(int j=i;j<=1000000;j=j+i)
+            {
+                if(prime_div[j]==0){
+                prime_div[j]=i;}
+                //cout<<j<<" "<<prime_div[j]<<endl;
+            }
+        }
+    }
+}
+int main()
+
+{
+    prime_sieve();
+    prime_div_min();
+   int t,n;
+   scanf("%d",&t);
+   while(t--)
+   {
+       scanf("%d",&n);
+       printf("%d\n",n-prime_div[n]);
+   }
+
+   return 0;
+}
