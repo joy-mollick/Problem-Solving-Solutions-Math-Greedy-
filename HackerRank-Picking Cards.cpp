ca62965379e00d270e9874e2822f46ea26f0f7fc
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-05-17 15:10:50 -0700

    Update HackerRank-Picking Cards.cpp

--- HackerRank-Picking Cards.cpp
+++ HackerRank-Picking Cards.cpp
@@ -1,3 +1,26 @@
+/*
+Just Do The Normal Product Formula First Calculate the frequency . And From 0 to n-1 calculate the sum of frequency of numbering of pick . And subtract one from sum , because one will be selected ,the remaining will be sum=sum-1;And Add the frequency of current numbering . And do multiplication of sum with product under modulo .Use long .That's all think about the example
+
+4 0 0 1 1
+
+the ans willl be the form 2*3*2*1
+
+Now if we calculate according our algorithm . Then , initial sum=0; try to traverse from 0 to 3 0-> sum=sum+freq(o)=0+2=2,one will be used .So ,now sum=sum-1=1; 1-> sum=sum+freq(1)=1+2=3, one will be used .So ,now sum=sum-1=2; 2->sum=sum+freq(2)= 2+0=2, one will be used .So ,now sum=sum-1=1; 3->sum=sum+freq(2)= 1+0=1, one will be used .So ,now sum=sum-1=0;
+
+                                                    product 2*3*2*1=12 .Same as we expect by practical .
+
+                                                    Be happy and Do code 
+
+
+
+
+
+
+*/
+
+
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
