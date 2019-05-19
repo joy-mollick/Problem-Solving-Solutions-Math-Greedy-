cc048f9acb38a00fa945c62dea4fd82385a35d6f
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-05-19 03:57:37 -0700

    Update HackerRank-Count Solutions.cpp

--- HackerRank-Count Solutions.cpp
+++ HackerRank-Count Solutions.cpp
@@ -1,3 +1,38 @@
+/*
+Given , x^2 + y^2 = (x*a) +(y*b) ;
+
+x^2 - x*a  + y^2 - y*b = 0; [ side change ]
+
+x^2 - 2 * (1/2) * (x) *a + (a/2)^2  - (a/2)^2 + y^2 - y*b =0 ;
+
+( x - (a / 2) ) ^2 =   (a/2)^2 - y^2 + y*b ;
+
+( x - (a / 2) ) ^2 =   (a^2 / 4) + y * (b - y) ;
+
+( x - (a / 2) ) ^2  =  ( a^2 + 4* y * (b - y) )/4;
+
+( x - (a / 2) )  = sqrt (   ( a^2 + 4* y * (b - y) )/4 );
+
+x  =   sqrt (   ( a^2 + 4* y * (b - y) )/4 )  +   (a / 2) ;
+
+x =    sqrt (   ( a^2 + 4* y * ( b - y )  )  )  +  a  ) / 2;
+
+x  =  ( (+-)sq  +a ) /2;  //   here sq holds for sqrt (   ( a^2 + 4* y * ( b - y )  )  ) 
+
+So  u have to care about 1) is sq positive ?otherwise there is no pair for y  
+
+ 2)  If  sq = 0 , then pairs are only one because then  (a+sq) and (a-sq) will be same value ,if sq !=0 then for one y there will be two pair .
+ 
+ 3)  Now   (a+sq)/2 is integer ? and between 1 and d ? then count.
+ 
+ 4) Another  (a-sq)/2  is integer ? and between 1 and d ? then count .
+ 
+ That's all .
+
+*/
+
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
