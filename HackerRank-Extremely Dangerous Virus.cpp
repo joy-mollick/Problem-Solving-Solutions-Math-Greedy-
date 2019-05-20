380a724db138f09224b3bc6f74b2573fc5f45178
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-05-20 05:11:40 -0700

    Update HackerRank-Extremely Dangerous Virus.cpp

--- HackerRank-Extremely Dangerous Virus.cpp
+++ HackerRank-Extremely Dangerous Virus.cpp
@@ -1,3 +1,23 @@
+/*
+GIven probability is 0.5 ( 1/2 ) . Factors are a and b . Initially cell is 1
+
+Every time ,it grows according expected value using factor given a & b
+
+fIrst time virus cell will be (a*(1/2)) + (b*(1/2)) = (a+b)/2;
+
+Now cells are (a+b)/2; Now this will be growing into again by factor a & b with probability (1/2) .Let's see ....................
+
+a*( ( a + b ) / 2 ) * ( 1 / 2 ) + b* ( ( a + b ) / 2 ) * ( 1 / 2 )
+
+= ( ( a + b ) / 2 ) * (( a + b ) / 2 ) ) , when time is 2 milisecond .
+
+So from this pattern , we can say ( ( a + b ) / 2 ) ^ t ....... %mod .
+
+
+
+*/
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
